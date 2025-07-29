70fa3cc9f0b11eed7253be35ca70b0cb6a78f153
Prajjwal Pathak <prajjwalpathak1105@gmail.com>
2025-07-29 20:56:59 +0530

    Add score placeholder

--- modules/script.js
+++ modules/script.js
@@ -289,15 +289,21 @@ canvas.width = window.innerWidth > window.innerHeight ? window.innerHeight / 1.4
 canvas.height = window.innerWidth > window.innerHeight ? window.innerHeight / 1.4 : window.innerWidth / 1.4;
 let unit = window.innerHeight < window.innerWidth ? window.innerHeight * 0.01 : window.innerWidth * 0.01;
 
-//Resize canvas everytime the window is resized
-window.addEventListener("resize", () => {
-    init();
-});
+// Game Components
+let start = false;
 
 const init = () => {
     canvas.width = window.innerWidth > window.innerHeight ? window.innerHeight / 1.4 : window.innerWidth / 1.4;
     canvas.height = window.innerWidth > window.innerHeight ? window.innerHeight / 1.4 : window.innerWidth / 1.4;
     unit = window.innerHeight < window.innerWidth ? window.innerHeight * 0.01 : window.innerWidth * 0.01;
+    start = false;
 }
 
 init();
+
+// Event-Listeners
+
+//Resize canvas everytime the window is resized
+window.addEventListener("resize", () => {
+    init();
+});
